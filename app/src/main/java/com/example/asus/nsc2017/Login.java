620fcfbaa19a25c96b726a31c4f66084808fe299
pasagorn <pasagorn_pg@kkumail.com>
2017-01-28 18:02:25 +0700

    no message

--- app/src/main/java/com/example/asus/nsc2017/Login.java
+++ app/src/main/java/com/example/asus/nsc2017/Login.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -28,6 +29,8 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
