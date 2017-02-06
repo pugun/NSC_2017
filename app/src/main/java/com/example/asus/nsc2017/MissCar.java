ae8b62c6b1f070fee3ee97cadbb1028d4f4a15bb
Nopphon <nopphonyel@gmail.com>
2017-02-06 21:25:16 +0700

    edit some xml file

--- app/src/main/java/com/example/asus/nsc2017/MissCar.java
+++ app/src/main/java/com/example/asus/nsc2017/MissCar.java
@@ -1,5 +1,6 @@
 package com.example.asus.nsc2017;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -18,12 +19,14 @@ public class MissCar extends MainActivity {
     ViewPager viewPager;
     CustomSwip customSwip;
     private PopupMenu mPopupMenu;
-    private static boolean isFirstTimeSync =true, isCreated =false;
+    private static boolean isFirstTimeSync = true, isCreated =false;
+    private static TextView name, birthDate, id, address, idcar, license ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_miss_car);
+        isCreated = true;
 
         viewPager = (ViewPager) findViewById(R.id.viewPager);
         customSwip = new CustomSwip(this);
@@ -55,8 +58,6 @@ public class MissCar extends MainActivity {
                 return true;
             }
         });
-
-        TextView name, birthDate, id, address, idcar, license ;
         name = (TextView) findViewById(R.id.name);
         birthDate = (TextView) findViewById(R.id.birthDate);
         id = (TextView) findViewById(R.id.id);
@@ -64,17 +65,49 @@ public class MissCar extends MainActivity {
         idcar = (TextView) findViewById(R.id.idcar);
         license = (TextView) findViewById(R.id.license);
 
+        if (isFirstTimeSync) startSync();
+    }
 
-
+    private static void setTex(){
         name.setText(StoreData.ownerModel.getName());
         birthDate.setText(StoreData.ownerModel.getBirthDate());
         id.setText(StoreData.ownerModel.getId());
         address.setText(StoreData.ownerModel.getAddress());
         license.setText(StoreData.getCurrentLicense());
-
     }
 
-    public static void finishedSync(){
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        isCreated = false;
+        isFirstTimeSync = true;
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        isCreated = false;
+        isFirstTimeSync = true;
+    }
+
+    static ProgressDialog fetchingDialog;
+
+    public static void finishedSync() {
+        if (isCreated) {
+            fetchingDialog.dismiss();
+            setTex();
+        }
+    }
+
+    private void startSync() {
+        if (isCreated) {
+            isFirstTimeSync = false;
+            fetchingDialog = new ProgressDialog(MissCar.this);
+            fetchingDialog.setCancelable(false);
+            fetchingDialog.setMessage("Fetching Data");
+            fetchingDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            fetchingDialog.setIndeterminate(true);
+            fetchingDialog.show();
+        }
     }
 }
