4b95f39b1ce07acc95f444e7ccc1027ac09c6be4
Nopphon <nopphonyel@gmail.com>
2017-02-06 13:47:48 +0700

    update logic with alert activity

--- app/src/main/java/com/example/asus/nsc2017/Alert.java
+++ app/src/main/java/com/example/asus/nsc2017/Alert.java
@@ -3,10 +3,13 @@ package com.example.asus.nsc2017;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class Alert extends AppCompatActivity {
+public class Alert extends AppCompatActivity implements View.OnClickListener{
 
     Intent intent;
+    ImageView lostBtn , thiefBtn ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +18,20 @@ public class Alert extends AppCompatActivity {
 
         boolean isLost = false, isThief = false;
         intent = getIntent();
+        isThief = intent.getBooleanExtra("THIEF" , false);
+        isLost = intent.getBooleanExtra("LOST" , false);
 
+        lostBtn = (ImageView) findViewById(R.id.lostCar);
+        thiefBtn = (ImageView) findViewById(R.id.thief);
+    }
+
+    public void setComponentVisible(boolean isLost , boolean isThief){
+        if(!isLost) lostBtn.setVisibility(View.GONE);
+        if(!isThief) thiefBtn.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onClick(View view) {
 
     }
 }
